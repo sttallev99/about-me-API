b7d4592a3ec1f9c3897706d7546294e6f2135daf
Георги Сталев <sttallev99@gmail.com>
2023-05-10 20:02:24 +0300

    create users and auth routes

--- src/index.js
+++ src/index.js
@@ -4,13 +4,23 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 require('dotenv').config();
 
+const userRoutes = require('./routes/users.routes');
+const authRoutes = require('./routes/auth.routes');
+
 const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-})
+});
 
-mongoose.connect(process.env.CONNECTION_STRING, {useNewUrlParser: true})
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+
+mongoose.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('DB Connected!'))
     .then(() => app.listen(5000, () => console.log('Server is running on port 5000')))
 

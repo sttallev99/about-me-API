8ce4cf76130c09366e75972d6b9d8284c946514c
Георги Сталев <sttallev99@gmail.com>
2023-05-10 21:12:09 +0300

    create register user route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,25 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
 
-router.get('/', (req, res) => {
-    res.send('Welcome to auth page')
+const User = require('../models/User');
+
+router.post('/register', async (req, res) => {
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+        });
+
+        const user = await newUser.save();
+        res.status(200).json(newUser)
+    } catch(err) {
+        console.log(err)
+    }
 })
 
 module.exports = router;

ee8fc2c37eb70a4ac0ff80b9941d4f8123da49f2
Георги Сталев <sttallev99@gmail.com>
2023-05-12 02:29:04 +0300

    fix follow user bug and implement unfollow user functionality

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -57,7 +57,7 @@ router.put('/:id/follow', async (req, res) => {
             const currentUser = await User.findById(req.body.userId);
             if(!user.followers.includes(req.body.userId)) {
                 await user.updateOne({ $push: { followers: req.body.userId }});
-                await currentUser.updateOne({ $push: { followings: req.body.userId }});
+                await currentUser.updateOne({ $push: { followings: req.params.id }});
                 res.status(200).json('user followed')
             } else {
                 res.status(403).json('you allready follow this user');
@@ -70,5 +70,22 @@ router.put('/:id/follow', async (req, res) => {
     }
 })
 //unfollow  user
+router.put('/:id/unfollow', async (req, res) => {
+    if(req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if(user.followers.includes(req.body.userId)) {
+                await user.updateOne({ $pull: { followers: req.body.userId }});
+                await currentUser.updateOne({ $pull: { followings: req.params.id }});
+                res.status(200).json('user unfollowed')
+            }
+        } catch(err) {
+
+        }
+    }else {
+        res.status(403).json('you cannot unfollow yourself');
+    }
+})
 
 module.exports = router;

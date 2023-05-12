40194639388e8081cdf8d59f25a855a0342afd4d
Георги Сталев <sttallev99@gmail.com>
2023-05-12 10:06:11 +0300

    implement create new post functionality

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,4 +1,21 @@
 const router = require('express').Router();
 
+const Post = require('../models/Post');
+
+//create a post
+router.post('/create', async(req, res) => {
+    const newPost = new Post(req.body);
+    try{
+        const savedPost = await newPost.save();
+        res.status(300).json(savedPost);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+})
+//update a post
+//delete a post
+//lika a post
+//get a post
+//get timeline posts
 
 module.exports = router;

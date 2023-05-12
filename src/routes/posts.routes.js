ded46bf2e0de617264f53b5a1972d0123f25c021
Георги Сталев <sttallev99@gmail.com>
2023-05-12 13:19:16 +0300

    implement get a post functionality

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -62,8 +62,17 @@ router.put('/:id/like', async (req, res) => {
     } catch(err) {
         res.status(500).json(err)
     }
-})
+});
+
 //get a post
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+})
 //get timeline posts
 
 module.exports = router;

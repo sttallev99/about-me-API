96f17e238e59527e55c6c7512c5b0b6a8e509d4e
Георги Сталев <sttallev99@gmail.com>
2023-05-12 12:56:39 +0300

    implement like post functionality

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -24,12 +24,44 @@ router.put('/:id', async (req, res) => {
             res.status(403).json('you can update only your posts');
         }
     } catch(err) {
-
+        res.status(500).json(err);
     }
 
-})
+});
+
 //delete a post
+router.delete('/:id', async (req, res) => {
+    try{
+       const post = await Post.findById(req.params.id);
+       if(req.body.userId === post.userId) {
+        await post.deleteOne();
+        res.status(200).json('post deleted');
+       }else {
+        res.status(403).json('you can delete only your post');
+       }
+    }catch(err) {
+        res.status(500).json(err)
+    }
+})
 //lika a post
+router.put('/:id/like', async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        console.log(post)
+        if(req.body.userId !== post.userId) {
+            if(!post.likes.includes(req.body.userId)) {
+                await post.updateOne({ $push: { likes: req.body.userId }});
+                res.status(200).json('post liked')
+            }else{
+                res.status(403).json('post was already liked')
+            }
+        }else {
+            res.status(403).json('You cannot like your post')
+        }
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
 //get a post
 //get timeline posts
 

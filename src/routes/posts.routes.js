28cd77df38b80fa2a48d05fa9c829be41c93e6fb
Георги Сталев <sttallev99@gmail.com>
2023-05-12 10:42:50 +0300

    implement updated post functionality

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -11,8 +11,23 @@ router.post('/create', async(req, res) => {
     } catch(err) {
         res.status(500).json(err);
     }
-})
+});
+
 //update a post
+router.put('/:id', async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        if(req.body.userId === post.userId) {
+           await post.updateOne(req.body);
+           res.status(200).json('post updated')
+        } else {
+            res.status(403).json('you can update only your posts');
+        }
+    } catch(err) {
+
+    }
+
+})
 //delete a post
 //lika a post
 //get a post
